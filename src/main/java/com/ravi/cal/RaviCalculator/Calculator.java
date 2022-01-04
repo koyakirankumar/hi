f8804a226e3ea2b5060ae1b5da3c8832c359ebab
koyakirankumar <96566195+koyakirankumar@users.noreply.github.com>
2022-01-04 12:05:17 +0530

    Update Calculator.java

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -39,24 +39,14 @@ public class Calculator  {
 	}
 	
 	public static void main(String[] args) {
+
+		long first = Long.parseLong(args[0]);
+		long second = Long.parseLong(args[1]);
 		
-		Calculator cc = new Calculator();
+		Calculator cal = new Calculator(first, second);
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter the first numeric value ");
-		long first = sc.nextLong();
-		cc.setFirst(first);
-		
-		System.out.println("Enter the second numeric value ");
-		long second = sc.nextLong();
-		cc.setSecond(second);
-		
-		System.out.println("The sum of two numbers is: "+cc.addFucn(first, second));
-		System.out.println("The difference of two numbers is: "+cc.subFucn(first, second));
-		System.out.println("The product of two numbers is: "+cc.mulFucn(first, second));
-		
-		
+		String output = String.format("\n*** Your Results ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct : %d\n\n", cal.first, cal.second, cal.addFucn(first, second), cal.subFucn(first, second), cal.mulFucn(first, second));
+		System.out.println(output);
 	}
 	 
 
